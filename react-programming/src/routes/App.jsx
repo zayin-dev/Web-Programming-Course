86e1e8da8d25d38a5ca788133674c7b2eed04529
zayin-dev <thejvides.official@outlook.com>
2025-04-29 12:45:36 +0000

    Adding new files to the project

--- react-programming/src/routes/App.jsx
+++ react-programming/src/routes/App.jsx
@@ -1,11 +1,36 @@
-import React from "react";
-import { createRoot } from 'react-dom/client'
+import React, { useContext } from "react";
 import '../App.css';
-import App from './Routes-Example'
-import { BrowserRouter } from 'react-router-dom'
+import { Routes, Route } from "react-routes-dom";
+import { AuthContext } from "./Login/AuthContext";
+import { Layout } from "./Login/Layout";
+import { Home } from './pages/Home';
+import { Contacts } from './pages/Contacts';
+import { Blogs } from './pages/Blog';
+import { Wikipedia } from './pages/Wikipedia';
+import { NoPage } from './pages/notFound';
+import { Login } from './Login/Login';
+import { OutletContent } from './pages/OutletContent';
+import { PublicLayout } from './pages/PublicLayout';
+import { Logout } from './pages/Logout' 
 
-createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-)
+const App = () => {
+    const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
+
+    return (
+        <Routes>
+            <Route element = { <PublicLayout/> }>
+                <Route path = "/"           element = { <Home /> }/>
+                <Route path = "/login"      element = { <Login /> }/>
+                <Route path = "/home"       element = { <Home /> }/>
+                <Route path = "/blogs"      element = { <Blogs /> }/>
+                <Route path = "/contacts"   element = { <Contacts /> }/>
+                <Route path = "/logout"     element = { <Logout /> }/>
+                <Route path = "*"           element = { <NoPage /> }/>
+            </Route>
+            
+
+            <Route></Route>
+        </Routes>
+    )
+}
+
